cdecee0553867e1c1597fa796e7eacd0623502d5
emilia0804 <187101155+emilia0804@users.noreply.github.com>
2025-08-10 11:32:45 +0630

    Setup Etch-a-Sketch

---
+++ Myscript.js
@@ -0,0 +1,42 @@
+let sizeBtn = document.querySelector("#SizeBtn");
+let container = document.querySelector("#container");
+
+sizeBtn.addEventListener("click",()=>{
+    let grid =parseInt(prompt("Enter number of squares per side for the new grid: "));
+    if(grid > 0 && grid < 100){
+        createGrid(grid);
+    }
+    else{
+        prompt("Enter a valid grid number between 1 and 99");
+    }
+});
+
+const defaultGrid = 16;
+createGrid(defaultGrid);
+
+function createGrid(grid){   
+    container.innerHTML = "";
+    let boxSize = 900 / grid;
+
+    for(let i=0;i<grid;i++){
+    for(let j=0;j<grid;j++){
+        let box = document.createElement("div");
+        box.classList.add("box");
+        box.style.width = `${boxSize}px`;
+        box.style.height = `${boxSize}px`;
+      
+        box.addEventListener("mouseover",()=>{
+            // box.classList.add("newBox");
+            let r = Math.floor(Math.random() * 256);
+            let g = Math.floor(Math.random() * 256);
+            let b = Math.floor(Math.random() * 256);
+            box.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+        });
+
+        container.appendChild(box);
+    }
+}
+}
+
+
+
